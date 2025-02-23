53cd1c5213dec36865987e8e02bb4dd35fb3fc71
Victor <almenamvictor@gmail.com>
2025-02-23 11:55:04 +0100

    add manejador de excepciones para que muestre mensajes en el postman

---
+++ src/main/java/org/example/grandao/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package org.example.grandao.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@ControllerAdvice
+
+public class GlobalExceptionHandler {
+
+    //Mensaje para las IlegalArgumenException
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+}
