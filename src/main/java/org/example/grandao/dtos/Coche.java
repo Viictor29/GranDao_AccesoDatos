2c96ec157d2511738033a7e014e6c24c0e296e97
Ruben <ruben.velasco2004@gmail.com>
2025-03-01 11:31:23 +0100

    Add javadoc to dto type classes

--- src/main/java/org/example/grandao/dtos/Coche.java
+++ src/main/java/org/example/grandao/dtos/Coche.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
+/**
+ * The type Coche.
+ */
 @Entity
 @XmlRootElement
 @Table(name = "coches", uniqueConstraints = @UniqueConstraint(columnNames = "matricula"))
@@ -24,40 +27,80 @@ public class Coche {
     private String modelo;
 
 
-    // Constructor vacío para JAXB
+    /**
+     * Instantiates a new Coche.
+     */
+// Constructor vacío para JAXB
     public Coche() {}
 
-    // Constructor con parámetros
+    /**
+     * Instantiates a new Coche.
+     *
+     * @param matricula the matricula
+     * @param marca     the marca
+     * @param modelo    the modelo
+     */
+// Constructor con parámetros
     public Coche(String matricula, String marca, String modelo) {
         this.matricula = matricula;
         this.marca = marca;
         this.modelo = modelo;
     }
 
-    // Getters y setters
+    /**
+     * Gets matricula.
+     *
+     * @return the matricula
+     */
+// Getters y setters
     @XmlElement
     public String getMatricula() {
         return matricula;
     }
 
+    /**
+     * Sets matricula.
+     *
+     * @param matricula the matricula
+     */
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
 
+    /**
+     * Gets marca.
+     *
+     * @return the marca
+     */
     @XmlElement
     public String getMarca() {
         return marca;
     }
 
+    /**
+     * Sets marca.
+     *
+     * @param marca the marca
+     */
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
+    /**
+     * Gets modelo.
+     *
+     * @return the modelo
+     */
     @XmlElement
     public String getModelo() {
         return modelo;
     }
 
+    /**
+     * Sets modelo.
+     *
+     * @param modelo the modelo
+     */
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
--- src/main/java/org/example/grandao/dtos/CochesList.java
+++ src/main/java/org/example/grandao/dtos/CochesList.java
@@ -4,25 +4,46 @@ import jakarta.xml.bind.annotation.XmlElement;
 
 import java.util.List;
 
+/**
+ * The type Coches list.
+ */
 public class CochesList {
 
     private List<Coche> coches;
 
-    // Constructor vacío
+    /**
+     * Instantiates a new Coches list.
+     */
+// Constructor vacío
     public CochesList() {
 
     }
 
-    // Constructor con lista de coches
+    /**
+     * Instantiates a new Coches list.
+     *
+     * @param coches the coches
+     */
+// Constructor con lista de coches
     public CochesList(List<Coche> coches) {
         this.coches = coches;
     }
 
+    /**
+     * Gets coches.
+     *
+     * @return the coches
+     */
     @XmlElement(name = "coche")
     public List<Coche> getCoches() {
         return coches;
     }
 
+    /**
+     * Sets coches.
+     *
+     * @param coches the coches
+     */
     public void setCoches(List<Coche> coches) {
         this.coches = coches;
     }
