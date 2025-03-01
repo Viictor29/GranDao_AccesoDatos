38ff0c1679b0226e3ea470738a2bcb934b991620
Ruben <ruben.velasco2004@gmail.com>
2025-03-01 11:31:45 +0100

    Add javadoc to dao type class "CocheXML"

--- src/main/java/org/example/grandao/daos/CocheXML.java
+++ src/main/java/org/example/grandao/daos/CocheXML.java
@@ -12,12 +12,21 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Coche xml.
+ */
 @Repository
 public class CocheXML {
 
     private static final String archivo = "coches.xml";
 
 
+    /**
+     * Leer coches list.
+     *
+     * @return the list
+     * @throws JAXBException the jaxb exception
+     */
     public List<Coche> leerCoches() throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(CochesList.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
@@ -25,6 +34,12 @@ public class CocheXML {
         return wrapper.getCoches();
     }
 
+    /**
+     * Guardar coches.
+     *
+     * @param listaCoches the lista coches
+     * @throws JAXBException the jaxb exception
+     */
     public void guardarCoches(List<Coche> listaCoches) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(CochesList.class);  // Usamos la clase CochesList como "wrapper"
         Marshaller marshaller = context.createMarshaller();
@@ -32,6 +47,13 @@ public class CocheXML {
         marshaller.marshal(new CochesList(listaCoches), new File(archivo));  // Convierte la lista de coches a XML y lo guarda en el archivo
     }
 
+    /**
+     * Leer coches por marca list.
+     *
+     * @param marca the marca
+     * @return the list
+     * @throws JAXBException the jaxb exception
+     */
     public List<Coche> leerCochesPorMarca(String marca) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(CochesList.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
@@ -46,6 +68,13 @@ public class CocheXML {
         return cochesFiltradosMarca;
     }
 
+    /**
+     * Leer coches por modelo list.
+     *
+     * @param modelo the modelo
+     * @return the list
+     * @throws JAXBException the jaxb exception
+     */
     public List<Coche> leerCochesPorModelo(String modelo) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(CochesList.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
