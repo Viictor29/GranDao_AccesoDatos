eb936b7c9e786cd114336b13e0171a85564f81f6
Victor <almenamvictor@gmail.com>
2025-03-01 11:03:19 +0100

    add DAO de CocheXML.class

---
+++ src/main/java/org/example/grandao/daos/CocheXML.java
@@ -0,0 +1,63 @@
+package org.example.grandao.daos;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+import org.example.grandao.dtos.Coche;
+import org.example.grandao.dtos.CochesList;
+import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CocheXML {
+
+    private static final String archivo = "coches.xml";
+
+
+    public List<Coche> leerCoches() throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(CochesList.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        CochesList wrapper = (CochesList) unmarshaller.unmarshal(new File(archivo));
+        return wrapper.getCoches();
+    }
+
+    public void guardarCoches(List<Coche> listaCoches) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(CochesList.class);  // Usamos la clase CochesList como "wrapper"
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);  // Configura el formato del XML
+        marshaller.marshal(new CochesList(listaCoches), new File(archivo));  // Convierte la lista de coches a XML y lo guarda en el archivo
+    }
+
+    public List<Coche> leerCochesPorMarca(String marca) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(CochesList.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        CochesList wrapper = (CochesList) unmarshaller.unmarshal(new File(archivo));
+
+        List<Coche> cochesFiltradosMarca = new ArrayList<>();
+        for (Coche coche : wrapper.getCoches()) {
+            if (coche.getMarca().equals(marca)) {
+                cochesFiltradosMarca.add(coche);
+            }
+        }
+        return cochesFiltradosMarca;
+    }
+
+    public List<Coche> leerCochesPorModelo(String modelo) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(CochesList.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        CochesList wrapper = (CochesList) unmarshaller.unmarshal(new File(archivo));
+
+        List<Coche> cochesFiltradosModelo = new ArrayList<>();
+        for (Coche coche : wrapper.getCoches()) {
+            if (coche.getMarca().equals(modelo)) {
+                cochesFiltradosModelo.add(coche);
+            }
+        }
+        return cochesFiltradosModelo;
+    }
+
+}
