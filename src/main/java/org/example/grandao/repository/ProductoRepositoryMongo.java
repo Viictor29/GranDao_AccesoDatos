7d71112ba1bbaabd76510f4145c289445773a2c5
Ruben <ruben.velasco2004@gmail.com>
2025-03-03 21:51:10 +0100

    Add Javadoc to dto "ProductoRepositoryMongo" for mongo repository

--- src/main/java/org/example/grandao/repository/ProductoRepositoryMongo.java
+++ src/main/java/org/example/grandao/repository/ProductoRepositoryMongo.java
@@ -4,6 +4,9 @@ import org.example.grandao.dtos.ProductoMongo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The interface Producto repository mongo.
+ */
 @Repository
 public interface ProductoRepositoryMongo extends MongoRepository<ProductoMongo, String> {
 }
