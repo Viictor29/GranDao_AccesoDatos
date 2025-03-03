b05a39cef4e745a580838549aec077cafabff958
Ruben <ruben.velasco2004@gmail.com>
2025-03-03 21:50:42 +0100

    Add Javadoc to dto "Producto" for txt

--- src/main/java/org/example/grandao/dtos/Producto.java
+++ src/main/java/org/example/grandao/dtos/Producto.java
@@ -1,24 +1,49 @@
 package org.example.grandao.dtos;
 
+/**
+ * The type Producto for txt.
+ */
 public class Producto {
     private int id;
     private String nombre;
     private double precio;
 
+    /**
+     * Instantiates a new Producto.
+     *
+     * @param id     the id
+     * @param nombre the nombre
+     * @param precio the precio
+     */
     public Producto(int id, String nombre, double precio) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets nombre.
+     *
+     * @return the nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Gets precio.
+     *
+     * @return the precio
+     */
     public double getPrecio() {
         return precio;
     }
